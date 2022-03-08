ce58d5e725a3ef207b9d5151fce204ba087b805b
Jhoel Perez <jhoel.jjpr@gmail.com>
2022-03-08 19:55:11 +0300

    review programs
    review programs and classes

---
+++ Review/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Hippo
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            //get number1
+            Console.WriteLine("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //get operator
+            Console.WriteLine("Operator: ");
+            string op = Console.ReadLine();
+
+            //get number2
+            Console.WriteLine("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //perform logic
+            if (op == "+") {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+        }
+    }
+}
---
+++ Review/Review.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+
+//we are on the hippo project 
+namespace Hippo
+{
+    //clas is a container where we can put code
+    class Program
+    {
+        //everything in Main gets executed 
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Hippo Hippo i__i ");
+            //String input = Console.ReadLine();
+            //for (int i=0; i<5; i++) {
+            //    Console.WriteLine(input);
+            //}
+
+
+            //String name = "Jhoel";
+            //int age = 25;
+            //Console.WriteLine("there once was a man named " + name);
+            //Console.WriteLine("He was " + age + " years old");
+            //Console.WriteLine("He really liked the name " + name);
+            //Console.WriteLine("But he really didn't like being " + age);
+
+            //-----TYPES
+            //string phrase = "Hippo Academy";
+            //char grade = 'A';
+            //int level = 9000;
+            //float depth = 10.9;
+            //double dous = 22.22;
+            //decimal money = 1.00000234;
+            //bool youSigma = true;
+
+            //-----STRINGS
+            //string phrase = "Hippo Academy";
+            //Console.WriteLine(phrase.Length);
+            //Console.WriteLine(phrase.ToUpper());
+            //Console.WriteLine(phrase.Contains("Hippo"));
+            //Console.WriteLine(phrase[0]);
+            //Console.WriteLine(phrase.Split(" ")[1]);
+            //Console.WriteLine(phrase.IndexOf('a'));
+            //Console.WriteLine(phrase.Substring(6,3));
+
+
+            //-----NUMBERS
+            //Console.WriteLine(Math.Abs(-100));
+            //Console.WriteLine(Math.Sqrt(81));
+            //Console.WriteLine(Math.Round(3.901));
+
+
+            //CALCULATOR
+            //Console.WriteLine("Enter a number: ");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter another number: ");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(number1 + number2);
+
+
+            //MAD LIB
+            //string color, pluralNoun, celebrity;
+            //Console.WriteLine("Enter a color: ");
+            //color = Console.ReadLine();
+
+            //Console.WriteLine("Enter a plural noun: ");
+            //pluralNoun = Console.ReadLine();
+
+            //Console.WriteLine("Enter a celebrity: ");
+            //celebrity = Console.ReadLine();
+
+            //Console.WriteLine("Roses are " + color);
+            //Console.WriteLine(pluralNoun + " are blue");
+            //Console.WriteLine("I Love " + celebrity);
+
+
+            //-----ARRAYS
+            //int[] luckyNumbers = {1,2,3,4 };
+            //string[] friends = new string [2];
+            //friends[0] = "Rachael";
+            //friends[1] = "Monica";
+            //Console.WriteLine(friends);
+
+            //-----METHODS
+            //hello()
+
+            //-----RETURN METHODS
+            //Console.WriteLine(cube(5));
+
+
+            //-----IF STATEMENTS
+            //bool is_male = true;
+            //bool is_tall = true;
+
+            //if (is_male && is_tall) { Console.WriteLine("you cannot bear children "); }
+            //else if (is_male && !is_tall) { Console.WriteLine("you are a short male "); }
+            //else if (!is_male && is_tall) { Console.WriteLine("you are not male but you are tall "); }
+            //else { Console.WriteLine("You are not male or tall"); }
+
+
+            //-----IF STATEMENTS COMPARISONS
+            //Console.WriteLine(getMax(2, 5, 8));
+
+        }
+
+        //static int getMax(int num1, int num2, int num3) {
+
+        //    if (num1 >= num2 && num1 >= num3){ return num1; }
+        //    else if (num2 >= num1 && num2 >= num3){ return num2; }
+        //    else { return num3; }
+
+        //    //return Math.Max(num1, num2);
+        //}
+
+        //static double cube(int num) {
+        //    double tmp = Math.Pow(Convert.ToDouble(num), Convert.ToDouble(3));
+        //    return tmp;
+        //}
+
+        //static void hello()
+        //{
+        //    string me = "Bob";
+        //    int age = 25;
+        //    SayHi(me, age);
+        //    SayHi(me, age);
+        //}
+        //static void SayHi(string name, int age) {
+        //    Console.WriteLine("Hello " + name + " you are " + age);
+        //}
+    }
+}
