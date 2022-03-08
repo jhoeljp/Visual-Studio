f6ecd58e7566092c6cb8e92a5e26cc70099d8b28
Jhoel Perez <jhoel.jjpr@gmail.com>
2022-03-08 17:02:58 +0300

    C# principles review
    simple review

---
+++ Review.cs
@@ -0,0 +1,81 @@
+using System;
+
+
+namespace Review
+{
+
+    class Program
+    {
+        //everything in Main gets executed 
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Hippo Hippo i__i ");
+            //String input = Console.ReadLine();
+            //for (int i=0; i<5; i++) {
+            //    Console.WriteLine(input);
+            //}
+
+
+            //String name = "Jhoel";
+            //int age = 25;
+            //Console.WriteLine("there once was a man named " + name);
+            //Console.WriteLine("He was " + age + " years old");
+            //Console.WriteLine("He really liked the name " + name);
+            //Console.WriteLine("But he really didn't like being " + age);
+
+            //-----TYPES
+            //string phrase = "Hippo Academy";
+            //char grade = 'A';
+            //int level = 9000;
+            //float depth = 10.9;
+            //double dous = 22.22;
+            //decimal money = 1.00000234;
+            //bool youSigma = true;
+
+            //-----STRINGS
+            //string phrase = "Hippo Academy";
+            //Console.WriteLine(phrase.Length);
+            //Console.WriteLine(phrase.ToUpper());
+            //Console.WriteLine(phrase.Contains("Hippo"));
+            //Console.WriteLine(phrase[0]);
+            //Console.WriteLine(phrase.Split(" ")[1]);
+            //Console.WriteLine(phrase.IndexOf('a'));
+            //Console.WriteLine(phrase.Substring(6,3));
+
+
+            //-----NUMBERS
+            //Console.WriteLine(Math.Abs(-100));
+            //Console.WriteLine(Math.Sqrt(81));
+            //Console.WriteLine(Math.Round(3.901));
+
+
+            //CALCULATOR
+            //Console.WriteLine("Enter a number: ");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter another number: ");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(number1 + number2);
+
+
+            //MAD LIB
+            //string color, pluralNoun, celebrity;
+            //Console.WriteLine("Enter a color: ");
+            //color = Console.ReadLine();
+
+            //Console.WriteLine("Enter a plural noun: ");
+            //pluralNoun = Console.ReadLine();
+
+            //Console.WriteLine("Enter a celebrity: ");
+            //celebrity = Console.ReadLine();
+
+            //Console.WriteLine("Roses are " + color);
+            //Console.WriteLine(pluralNoun + " are blue");
+            //Console.WriteLine("I Love " + celebrity);
+
+
+            //-----ARRAYS
+
+
+        }
+    }
+}
